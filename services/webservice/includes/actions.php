0511550632c5734f58787f2222b7e51a34ccd5ee
jaimyvdb03 <jaimyvdb03@gmail.com>
2024-09-16 15:29:09 +0200

    services

---
+++ services/webservice/includes/actions.php
@@ -0,0 +1,70 @@
+ <?php
+/**
+ * @return array
+ */
+function getArticle()
+{
+    return [
+        [
+            "id" => 1,
+            "name" => "Tito Jackson (70), lid van Jackson 5, overleden",
+            "image" => "",
+            "link" => "https://nos.nl/artikel/2537306-tito-jackson-70-lid-van-jackson-5-overleden",
+            "status" => ""
+        ],
+        [
+            "id" => 2,
+            "name" => "Jumbo",
+            "image" => "",
+            "link" => "",
+            "status" => ""
+        ],
+        [
+            "id" => 3,
+            "name" => "Lidl",
+            "image" => "",
+            "link" => "",
+            "status" => ""
+        ],
+        [
+            "id" => 4,
+            "name" => "Plus",
+            "image" => "",
+            "link" => "",
+            "status" => ""
+        ],
+        [
+            "id" => 5,
+            "name" => "Spar",
+            "image" => "",
+            "link" => "",
+            "status" => ""
+        ],
+        [
+            "id" => 6,
+            "name" => "Coop",
+            "image" => "",
+            "link" => "",
+            "status" => ""
+        ],
+        [
+            "id" => 7,
+            "name" => "Dirk",
+            "image" => "",
+            "link" => "",
+            "status" => ""
+        ],
+        [
+            "id" => 8,
+            "name" => "Hoogvliet",
+            "image" => "",
+            "link" => "",
+            "status" => ""
+        ],
+    ];
+}
+
+/**
+ * @param $id
+ * @return mixed
+ */
