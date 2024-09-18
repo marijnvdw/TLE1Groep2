f2bf9cf3acac5fe40febc86226ef27bd98652fe1
David de Knegt <daviddeknegt@gmail.com>
2024-09-18 10:45:37 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 window.addEventListener('load', init);
+
+fetch('index.php')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        // Gebruik de data hier in je JavaScript-bestand
+    })
+    .catch(error => console.error('Error:', error));
