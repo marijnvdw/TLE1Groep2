44353ae61d04ea7f192204af6b82bca5692e7dff
SemvanOsch <145602882+SemvanOsch@users.noreply.github.com>
2024-09-25 10:49:33 +0200

    data gaat nu naar de database

--- php/insert.php
+++ php/insert.php
@@ -6,18 +6,19 @@ session_start();
 // Setup connection with database
 require_once 'database.php';
 
-$score = $_GET['score'];
-$response = $_GET['response'];
+var_dump($_POST);
 
+    $url = mysqli_real_escape_string($db, $_POST['url']);
+    $score = mysqli_real_escape_string($db, $_POST['score']);
+    $response = mysqli_real_escape_string($db, $_POST['response']);
+
+$query = "INSERT INTO weblink (link, score, response) VALUES('$url', '$score', '$response')";
+
+    if (mysqli_query($db, $query)) {
+        echo "toegevoegd";
+    } else {
+        echo "Error: " . $query . "<br>" . mysqli_error($db);
+    }
+
+mysqli_close($db);
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<body>
-    <?= $score ?>
-    <br><br><br><br>
-    <?= $response ?>
-</body>
-
-</html>
